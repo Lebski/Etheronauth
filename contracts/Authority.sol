e6a4fb60df1a9d667ce045f554fc0fc2ed8bc42a
Felix <email@felixleber.de>
2018-03-14 12:52:49 +0100

    Renamed

---
+++ contracts/Authority.sol
@@ -0,0 +1,102 @@
+pragma solidity ^0.4.17;
+
+
+contract Authority {
+    struct Levelobj {
+        uint write;
+        uint read;
+    }
+
+    struct Permissionrequest {
+        bytes32 alg;
+        bytes32 typ;
+        bytes32 iss;
+        uint sub;
+        uint audience;
+        uint exp;
+        uint nbf;
+        uint iat;
+        uint jti;
+    }
+
+    address public owner;
+    uint public lastCompletedMmigration;
+    uint private jtiCounter;
+    mapping (address => Levelobj) public permissions;
+    mapping (bytes32 => Permissionrequest) public permissionList;
+
+    event PermissionRequestdeployed (bytes32 permissionId);
+    event PermissionGranted (bytes32 permissionId);
+
+    modifier restricted() {
+        if (msg.sender == owner) _;
+    }
+
+    function Authority() public {
+        owner = msg.sender;
+        jtiCounter = 0;
+    }
+
+    function addPermissionRequest (
+        //sender ID
+        bytes32 _permissionId,
+        bytes32 _alg,
+        bytes32 _typ,
+        bytes32 _iss,
+        uint _sub,
+        uint _audience,
+        uint _exp,
+        uint _nbf,
+        uint _iat) public {
+        permissionList[_permissionId] = Permissionrequest(
+        _alg, _typ, _iss, _sub, _audience, _exp, _nbf, _iat, jtiCounter++);
+        PermissionRequestdeployed(_permissionId);
+    }
+
+    /*
+    * DOESNT WORK BECAUSE "Stack is too deep"
+    function getRequest(bytes32 _permissionId) public view returns(
+        bytes32 alg, bytes32 typ, bytes32 iss, uint sub, uint audience, uint exp, uint nbf, uint iat, uint jti) {
+        Permissionrequest storage request = permissionList[_permissionId];
+        return (request.alg, request.typ, request.iss, request.sub,
+        request.audience, request.exp, request.nbf, request.iat, request.jti);
+    }
+    */
+    function getRequestAlg(bytes32 _permissionId) public view returns(bytes32) {
+        return permissionList[_permissionId].alg;
+    }
+
+    function getRequestTyp(bytes32 _permissionId) public view returns(bytes32) {
+        return permissionList[_permissionId].typ;
+    }
+
+    function getRequestIss(bytes32 _permissionId) public view returns(bytes32) {
+        return permissionList[_permissionId].iss;
+    }
+
+    function getRequestSub(bytes32 _permissionId) public view returns(uint) {
+        return permissionList[_permissionId].sub;
+    }
+
+    function getRequestAud(bytes32 _permissionId) public view returns(uint) {
+        return permissionList[_permissionId].audience;
+    }
+
+    function getRequestExp(bytes32 _permissionId) public view returns(uint) {
+        return permissionList[_permissionId].exp;
+    }
+
+    function getRequestNbf(bytes32 _permissionId) public view returns(uint) {
+        return permissionList[_permissionId].nbf;
+    }
+
+    function getRequestIat(bytes32 _permissionId) public view returns(uint) {
+        return permissionList[_permissionId].iat;
+    }
+
+    function getRequestJti(bytes32 _permissionId) public view returns(uint) {
+        return permissionList[_permissionId].jti;
+    }
+
+
+}

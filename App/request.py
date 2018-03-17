3823557d7a57ad8026bcf9b529b53e51535e9762
Felix <email@felixleber.de>
2018-03-17 17:38:09 +0100

    Added store_signature with storeOnBlockchain.py Added packet funct

--- App/request.py
+++ App/request.py
@@ -4,6 +4,8 @@ import web3
 from web3 import Web3, HTTPProvider, TestRPCProvider
 from solc import compile_source
 
+from verifier import storeOnBlockchain
+
 def setup_Web3():
     global web3
     web3 = Web3(HTTPProvider('http://localhost:8545'))
@@ -20,28 +22,45 @@ def get_Contract(contract_interface, contract_address):
         return deployed_Contract
 
 def make_request(deployed_Contract):
+
     print("Using Contract at contract at > {}\n".format(deployed_Contract.address))
 
     ############## only for debugging ##################
     testhash = '0xaba'
-    testbytes = "hi"
     ####################################################
 
     hex_bytes = Web3.toBytes(hexstr=testhash)
-    text_bytes = Web3.toBytes(text="testhash")
+    text_bytes = Web3.toBytes(text=testhash)
     print(text_bytes)
-    deployed_Contract.functions.addPermissionRequest(hex_bytes, text_bytes, text_bytes, web3.eth.coinbase , 4, 4, 4, 4, 4).transact({'from': web3.eth.coinbase})
+    deployed_Contract.functions.addPermissionRequest(text_bytes, text_bytes, text_bytes, web3.eth.coinbase , 4, 4, 4, 4, 4).transact({'from': web3.eth.coinbase})
 
     ############## Filter nicht verfügbar ##############
     #transfer_filter = deployed_Contract.eventFilter('PermissionRequestdeployed', {'filter': {'_from': '0xD6C35F62F6Ca97A28f175078CD47Ce62bF74de36'}})
     #print(transfer_filter.get_all_entries())
     ####################################################
 
-    [alg, typ, iss, sub, audience, exp, nbf, iat, jti, signature] = deployed_Contract.functions.permissionList(hex_bytes).call(
+    [alg, typ, iss, sub, audience, exp, nbf, iat, jti, signature] = deployed_Contract.functions.permissionList(text_bytes).call(
         {'from': web3.eth.coinbase})
 
     print('Erg: \n Algo:{}\n Typ:{}\n Issue:{}\n Subject:{}\n Audience:{}\n Expiration:{}\n NotBefore:{}\n Issued at:{}\n Nonce:{}\n Signature:{}\n'.format(Web3.toText(alg), Web3.toText(typ), iss, sub, audience, exp, nbf, iat, jti, Web3.toText(signature)))
 
+def read_request(deployed_Contract):
+    print("Reading Contract at contract at > {}\n".format(deployed_Contract.address))
+
+    ############## only for debugging ##################
+    testhash = '0xaba'
+    text_bytes = Web3.toBytes(text=testhash)
+    ####################################################
+
+
+
+    [alg, typ, iss, sub, audience, exp, nbf, iat, jti, signature] = deployed_Contract.functions.permissionList(text_bytes).call(
+        {'from': web3.eth.coinbase})
+
+    print('Erg: \n Algo:{}\n Typ:{}\n Issue:{}\n Subject:{}\n Audience:{}\n Expiration:{}\n NotBefore:{}\n Issued at:{}\n Nonce:{}\n Signature:{}\n'.format(Web3.toText(alg), Web3.toText(typ), iss, sub, audience, exp, nbf, iat, jti, Web3.toText(signature)))
+
+
+### if return of hexstr --> call print(hexstrngwatear) without format!!!
 
 setup_Web3()
 data = get_Ressources('metadata.json')
@@ -49,3 +68,5 @@ interface = get_Ressources('interface.json')
 voting_interface = interface
 deployed_Contract = get_Contract(voting_interface, data["parameter"]["address"])
 make_request(deployed_Contract)
+storeOnBlockchain.store_signature('0xaba', '0xaba')
+read_request(deployed_Contract)
---
+++ App/verifier/storeOnBlockchain.py
@@ -0,0 +1,64 @@
+import json
+import web3
+
+from web3 import Web3, HTTPProvider, TestRPCProvider
+from solc import compile_source
+
+def setup_Web3():
+    global web3
+    web3 = Web3(HTTPProvider('http://localhost:8545'))
+    print("sucessfully set up Web3 Env to Account: ", web3.eth.accounts[0])
+
+
+def get_Ressources(filename):
+    data = json.load(open(filename))
+    return data
+
+def get_Contract(contract_interface, contract_address):
+        deployed_Contract = web3.eth.contract(
+            abi=contract_interface['abi'], address=contract_address)
+        return deployed_Contract
+
+def make_request(deployed_Contract):
+    print("Using Contract at contract at > {}\n".format(deployed_Contract.address))
+
+    ############## only for debugging ##################
+    testhash = '0xaba'
+    testbytes = "hi"
+    ####################################################
+
+    hex_bytes = Web3.toBytes(hexstr=testhash)
+    text_bytes = Web3.toBytes(text="testhash")
+    print(text_bytes)
+    deployed_Contract.functions.addPermissionRequest(hex_bytes, text_bytes, text_bytes, web3.eth.coinbase , 4, 4, 4, 4, 4).transact({'from': web3.eth.coinbase})
+
+    ############## Filter nicht verfügbar ##############
+    #transfer_filter = deployed_Contract.eventFilter('PermissionRequestdeployed', {'filter': {'_from': '0xD6C35F62F6Ca97A28f175078CD47Ce62bF74de36'}})
+    #print(transfer_filter.get_all_entries())
+    ####################################################
+
+    [alg, typ, iss, sub, audience, exp, nbf, iat, jti, signature] = deployed_Contract.functions.permissionList(hex_bytes).call(
+        {'from': web3.eth.coinbase})
+
+    print('Erg: \n Algo:{}\n Typ:{}\n Issue:{}\n Subject:{}\n Audience:{}\n Expiration:{}\n NotBefore:{}\n Issued at:{}\n Nonce:{}\n Signature:{}\n'.format(Web3.toText(alg), Web3.toText(typ), iss, sub, audience, exp, nbf, iat, jti, Web3.toText(signature)))
+
+
+#To be tested
+def store_signature(permission_id, signature):
+    setup_Web3()
+    data = get_Ressources('metadata.json')
+    interface = get_Ressources('interface.json')
+    voting_interface = interface
+    deployed_Contract = get_Contract(voting_interface, data["parameter"]["address"])
+    permission_id_bytes = Web3.toBytes(text=permission_id)
+    signature_bytes = Web3.toBytes(text=signature)
+    deployed_Contract.functions.storeSignature(permission_id_bytes, signature_bytes).transact({'from': web3.eth.coinbase})
+
+
+if __name__ == "__main__":
+    setup_Web3()
+    data = get_Ressources('metadata.json')
+    interface = get_Ressources('interface.json')
+    voting_interface = interface
+    deployed_Contract = get_Contract(voting_interface, data["parameter"]["address"])
+    make_request(deployed_Contract)

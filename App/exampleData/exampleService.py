649499d3df27c804bb0343fc99e7139473cd777a
Felix <email@felixleber.de>
2018-03-19 18:13:25 +0100

    example use-case for testing

--- App/exampleData/exampleService.py
+++ App/exampleData/exampleService.py
@@ -1,7 +1,10 @@
 import requests
+import json
 url = 'http://127.0.0.1:5000'
 
-data = '{ "payload":{ "iss":"0.2",      "sub":"0x93794c6778F1C64b0f7CFBE1A7271c1749D8707d",      "aud":"0.2",      "exp":"0x93794c6778F1C64b0f7CFBE1A7271c1749D8707d",      "nbf":"0.2",      "iat":"0x93794c6778F1C64b0f7CFBE1A7271c1749D8707d",      "jti":"0.2"    },    "signature": "" }'
+data = json.load(open("exampleData/examplePermissions.json"))
+file = open("exampleData/examplePermissions.json", "r")
+data =  file.read()
 
 header={ "Content-Type": "application/json" }
 response = requests.post(url, data=data, headers=header)

f5452c0382ff71b8fc90fab05c5d8c020b40d0a7
Felix <email@felixleber.de>
2018-03-19 18:11:51 +0100

    Added REST communication

--- App/etheronauthAPI.py
+++ App/etheronauthAPI.py
@@ -1,19 +1,30 @@
 import json
 
 from flask import Flask, request
+from etheronauth import storeOnBlockchain
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        handle_input(request.get_json(force=True))
+        permission_id = handle_input(request.get_json(force=True))
+        #return permission_id.hex()
+
     else:
         "please provide your input"
     return("Thank you for your participation")
 
 def handle_input(request_json):
-    print(request_json['payload']['sub'])
+    sub = request_json['payload']['sub']
+    exp = request_json['payload']['sub']
+    nbf = request_json['payload']['sub']
+    iat = request_json['payload']['sub']
+    audience = request_json['payload']['sub']
+    permission_id = storeOnBlockchain.make_request(sub=sub, audience=audience, exp=exp, nbf=nbf, iat=iat)
+    return permission_id
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)

6f8ed73bdff3e5678f0ed6a4c6e6c3e7bbcf6c5c
Felix <email@felixleber.de>
2018-03-20 19:59:43 +0100

    Add read functionality

--- App/etheronauthAPI.py
+++ App/etheronauthAPI.py
@@ -1,15 +1,16 @@
 import json
+import flask
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from etheronauth import storeOnBlockchain
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def login():
+@app.route('/makeRequest', methods=['GET', 'POST'])
+def make_Request():
     if request.method == 'POST':
         permission_id = handle_input(request.get_json(force=True))
-        #return permission_id.hex()
+        return permission_id.hex()
 
     else:
         "please provide your input"
@@ -22,9 +23,15 @@ def handle_input(request_json):
     iat = request_json['payload']['sub']
     audience = request_json['payload']['sub']
     permission_id = storeOnBlockchain.make_request(sub=sub, audience=audience, exp=exp, nbf=nbf, iat=iat)
+    #store_token(permission_id.hex())
     return permission_id
 
-
+@app.route('/readToken', methods=['GET', 'POST'])
+def read_token(permission_id=None):
+    permission_id = request.data.decode('utf-8')
+#    response = flask.jsonify(storeOnBlockchain.read_request(permission_id))
+    response = storeOnBlockchain.read_request(permission_id)
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)

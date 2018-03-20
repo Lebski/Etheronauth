ec1ec0344516f5f0dff9ac8b7048a719ff30f46d
Felix <email@felixleber.de>
2018-03-20 20:00:47 +0100

    Updated read_request functionality and return value [json-str]

--- App/etheronauth/storeOnBlockchain.py
+++ App/etheronauth/storeOnBlockchain.py
@@ -7,6 +7,7 @@ from solc import compile_source
 def setup_Web3():
     global web3
     web3 = Web3(HTTPProvider('http://localhost:8545'))
+    #errorhandling!
     print("sucessfully set up Web3 Env to Account: ", web3.eth.accounts[0])
 
 
@@ -20,19 +21,17 @@ def get_Contract(contract_interface, contract_address):
         return deployed_Contract
 
 def make_request(sub=0, audience=0, exp=0, nbf=0, iat=0):
-    
+    sub = int(sub)
+    exp = int(exp)
+    nbf = int(nbf)
+    iat = int(iat)
+    audience = int(audience)
     print("Using Contract at contract at > {}\n".format(deployed_Contract.address))
     permission_id = Web3.soliditySha3(['address', 'uint256', 'uint256', 'uint256', 'uint256', 'uint256'], [web3.eth.coinbase, sub, audience, exp, nbf, iat])
     alg = Web3.toBytes(text="RS256")
     typ = Web3.toBytes(text="JWT")
 
     deployed_Contract.functions.addPermissionRequest(permission_id, alg, typ, sub, audience, exp, nbf, iat).transact({'from': web3.eth.coinbase})
-
-
-    ############## Filter nicht verfügbar ##############
-    #transfer_filter = deployed_Contract.eventFilter('PermissionRequestdeployed', {'filter': {'_from': '0xD6C35F62F6Ca97A28f175078CD47Ce62bF74de36'}})
-    #print(transfer_filter.get_all_entries())
-    ####################################################
 
     [alg, typ, iss, verifier,sub, audience, exp, nbf, iat, jti, signature] = deployed_Contract.functions.permissionList(permission_id).call(
         {'from': web3.eth.coinbase})
@@ -45,13 +44,46 @@ def read_request(permission_id):
     [alg, typ, iss, verifier,sub, audience, exp, nbf, iat, jti, signature] = deployed_Contract.functions.permissionList(permission_id).call(
         {'from': web3.eth.coinbase})
 
-    print('Issued JWT-Token: \n Algo:{}\n Typ:{}\n Issue:{}\n Verifier:{}\n Subject:{}\n Audience:{}\n Expiration:{}\n NotBefore:{}\n Issued at:{}\n Nonce:{}\n Signature:{}\n'.format(Web3.toText(alg), Web3.toText(typ), iss, verifier,sub, audience, exp, nbf, iat, jti, Web3.toText(signature)))
+    print('Load JWT-Token from Blockchain: \n Algo:{}\n Typ:{}\n Issue:{}\n Verifier:{}\n Subject:{}\n Audience:{}\n Expiration:{}\n NotBefore:{}\n Issued at:{}\n Nonce:{}\n Signature:{}\n'.format(Web3.toText(alg), Web3.toText(typ), iss, verifier,sub, audience, exp, nbf, iat, jti, Web3.toText(signature)))
+
+    #### Sorry, web3 don't remove the padding    ####
+    #### ... so doing it manually                ####
+    typ = typ.split(b'\0',1)[0]
+    alg = alg.split(b'\0',1)[0]
+    signature = signature.split(b'\0',1)[0]
+
+    #### Byte-String to text                     ####
+    typ = Web3.toText(typ)
+    alg = Web3.toText(alg)
+    signature = Web3.toText(signature)
+
+    token = {
+       "header":{
+          "typ": typ,
+          "alg": alg
+       },
+       "payload":{
+         "iss": iss,
+         "sub": sub,
+         "verifier": verifier,
+         "aud": audience,
+         "exp": exp,
+         "nbf": nbf,
+         "iat": iat,
+         "jti": jti,
+       },
+       "signature": signature
+    }
+    #### Returns Token of type 'dict'            ####
+    json_token = json.dumps(token)
+    return json_token
 
 def store_signature(permission_id, signature):
 
     signature_bytes = Web3.toBytes(text=signature)
     deployed_Contract.functions.storeSignature(permission_id, signature_bytes).transact({'from': web3.eth.coinbase})
 
+### INITAL SETUP ###
 setup_Web3()
 data = get_Ressources('metadata.json')
 interface = get_Ressources('interface.json')

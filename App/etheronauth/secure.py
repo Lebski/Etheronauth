1c233a07dc8e4ced723b5f6f65790cbd4b7fc647
Felix <email@felixleber.de>
2018-04-10 23:59:12 +0200

    Added file encryption

--- App/etheronauth/secure.py
+++ App/etheronauth/secure.py
@@ -21,31 +21,26 @@ def generate_key(pw_string, salt=b"salt_etheronauth"):
     password = str.encode(pw_string)
     kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), length=32, salt=salt, iterations=100000, backend=default_backend())
     key = base64.urlsafe_b64encode(kdf.derive(password))
-    print (key)
     return key
 
 def get_salt():
     return os.urandom(16)
 
-
-if __name__ == "__main__":
-    print("This is the testing func")
-    key = Fernet.generate_key()
+def read_bytes_from_file(pw_string, filepath):
+    key = generate_key(pw_string)
     fernet_obj = Fernet(key)
-    input = str.encode("Hello Hello Major Timo")
-    token = encrypt(fernet_obj, input)
-    print("Testing Encryption with string: \"Hello Hello Major Timo\"")
-    print("Returns {}".format(token))
-    output = decrypt(fernet_obj, token)
-    print("Decrypts to {}".format(output))
+    pw_File = open(filepath, "rb")
+    data = pw_File.read()
+    pw_File.close()
+    file = decrypt(fernet_obj, data)
+    return file
 
-    #salt = get_salt()
-    key = generate_key("Hallo")
+def write_bytes_to_file(pw_string, filepath, data):
+    key = generate_key(pw_string)
     fernet_obj = Fernet(key)
-    input = str.encode("Hello Hello Major Timo")
-    token = encrypt(fernet_obj, input)
-    print("Testing Encryption with string: \"Hello Hello Major Timo\"")
-    print("Returns {}".format(token))
-    key = generate_key("Hallo")
-    output = decrypt(fernet_obj, token)
-    print("Decrypts to {}".format(output))
+    enc_file = encrypt(fernet_obj, data)
+    out_file = open(filepath, "wb")
+    out_file.write(enc_file)
+    out_file.close()
+
+#def read_json_from_file():
---
+++ App/testing/testSecretHandling.py
@@ -0,0 +1,35 @@
+import json
+
+from etheronauth import secure
+from cryptography.fernet import Fernet
+
+# Testing basic decryption/encryption
+print("This is the testing func")
+key = Fernet.generate_key()
+fernet_obj = Fernet(key)
+data = str.encode("Hello Hello Major Timo")
+token = secure.encrypt(fernet_obj, data)
+print("Testing Encryption with string: \"Hello Hello Major Timo\"")
+print("Returns {}".format(token))
+output = secure.decrypt(fernet_obj, token)
+print("Decrypts to {}".format(output))
+
+# Testing Key-Generation with passwords
+#salt = get_salt()
+key = secure.generate_key("Hallo")
+fernet_obj = Fernet(key)
+input = str.encode("Hello Hello Major Timo")
+token = secure.encrypt(fernet_obj, input)
+print("Testing Encryption with string: \"Hello Hello Major Timo\"")
+print("Returns {}".format(token))
+key = secure.generate_key("Hallo")
+output = secure.decrypt(fernet_obj, token)
+print("Decrypts to {}".format(output))
+
+
+#From now on don't use the cryptography package
+
+# Testing write/read files
+secure.write_bytes_to_file("munckintime", "testing/testPassword", b"5uP3R53CR37P55wRD")
+data = secure.read_bytes_from_file("munckintime", "testing/testPassword")
+print("Passwordfile contains: {}".format(data))

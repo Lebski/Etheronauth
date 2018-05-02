abdb508d36decf5c776ebb92237738adaa12a5ce
Felix <email@felixleber.de>
2018-05-02 15:44:37 +0200

    Rebased web3login

--- tests/test_web3login.py
+++ tests/test_web3login.py
@@ -1,16 +1,16 @@
 import json
-from etheronauth import chainhandler
+from etheronauth import web3login
 from etheronauth import log
 
-log.out.info("### Testing on module > chainhandler <")
+log.out.info("### Testing on module > web3login <")
 log.out.info("# Checking on web3_login")
-account_test1 = chainhandler.web3_login()
+account_test1 = web3login.web3_login()
 log.out.info("Account: {}".format(account_test1))
 log.out.info("# Checking on web3_logout")
-chainhandler.web3_logout(account_test1)
+web3login.web3_logout(account_test1)
 log.out.info("# Logged out.")
 log.out.info("# Checking on web3_silent_login")
-account_test2 = chainhandler.web3_silent_login(account_test1, "TestTestTest")
+account_test2 = web3login.web3_silent_login(account_test1, "TestTestTest")
 log.out.info("Account: {}".format(account_test2))
-chainhandler.web3_logout(account_test2)
+web3login.web3_logout(account_test2)
 log.out.info("# Logged out.")
